e62eba6fe7609afce1c078d13489f9a39d62fb0b
Dev Sharma <devsharma1683@gmail.com>
2025-04-10 14:03:56 +0530

    setup store and authSlice, added testing components, added provider in main, and setup basic functions in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,42 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import { useDispatch } from "react-redux";
 import './App.css'
+import authService from './appwrite/auth';
+import { login, logout } from './store/authSlice';
+import { Footer, Header } from './components/index';
 
 function App() {
-  
-  console.log(import.meta.env.VITE_APPWRITE_URL) 
-  
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>Blog app with appwrite</h1>
-    </>
-  )
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }
+    })
+    .finally(()=>{setLoading(false)})
+  },[])
+  
+  // Conditional Redering typw :01
+  // if(loading){
+  //   return <div>Loading...</div>
+  // }
+
+  // Conditional Redering typw :02
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400' >
+      <div className="w-full block">
+        <Header/>
+          <main>
+            {/* <Outlet/> */}
+          </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
+
 }
 
 export default App
