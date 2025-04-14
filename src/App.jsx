c698b49acdd15224631cf1c0a72aefeca57af9c8
Dev Sharma <devsharma1683@gmail.com>
2025-04-14 18:25:18 +0530

    comment out some text

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
       <div className="w-full block">
         <Header/>
           <main>
-            TODO:<Outlet/>
+            {/*TODO:*/}<Outlet/>
           </main>
         <Footer/>
       </div>
