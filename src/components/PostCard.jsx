29e56667495a4d8ec1ae18b02bdeae7ea97fc6c1
Dev Sharma <devsharma1683@gmail.com>
2025-04-15 00:19:37 +0530

    some style changes

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -14,10 +14,10 @@ function PostCard(post){
 
     return(
         <Link to={`/post/${$id}`}>
-            <div className="w-full bg-gray-100 rounded-xl p-4 ">
-                <div className="w-full justify-center mb-4">
+            <div className="w-full bg-white rounded-2xl shadow-md hover:shadow-lg transition p-4 space-y-2 ">
+                <div className="w-full justify-center mb-4 ">
                     {/* <img src={appwriteService.getFilePreview(featuredImage)} alt={title} className="rounded-xl w-full h-48 object-cover-xl"/> */}
-                    <img src={imageUrl} alt={title} className="rounded-xl w-full h-48 object-cover-xl"/>
+                    <img src={imageUrl} alt={title} className="rounded-xl w-full h-48 object-cover-xl shadow-md"/>
                 </div>
                 <h2 className="text-xl font-bold">{title}</h2>
             </div>
