4a3c28f6e062a9dd2a53b6acf4d2ae131fbde1c7
Dev Sharma <devsharma1683@gmail.com>
2025-06-12 20:00:11 +0530

    added vercel analytics tool

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import authService from './appwrite/auth';
 import { login, logout } from './store/authSlice';
 import { Footer, Header } from './components/index';
 import { Outlet } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   const [loading, setLoading] = useState(true)
@@ -36,6 +37,7 @@ function App() {
           </main>
         <Footer/>
       </div>
+      <Analytics />
     </div>
   ) : null
 
